80786661509c919d51c676be3b78eefd733b7a94
Hank Beaver <hbeaver@gmail.com>
2015-03-19 09:39:54 -0400

    wip

--- spec/serial_spec/provides_spec.rb
+++ spec/serial_spec/provides_spec.rb
@@ -3,30 +3,52 @@ require 'spec_helper'
 describe "SerialSpec::RequestResponse::Provide" do
   include SerialSpec::RequestResponse::Provide
 
-  ## Q:
-  ## 1.does matcher needs to be aware of type being matched again? i.e. expects argument of "body"
-  let(:post) { Post.new }
   let(:comments) do 
     [Comment.new(:title => "Comment1"), Comment.new(:title => "Comment2")]
   end
+  let(:user) { User.new(name: "Enrique") }
+  let(:post) do
+    Post.new(:title => "New Post", :body => "Body of new post")
+  end
+
+  #let(:post) do 
+    #p = Post.new(:title => "New Post", :body => "Body of new post")
+    #p.comments = comments
+    #p.author = user
+    #p
+  #end
 
   let(:resource_serializer)  do
     resource.as_json
   end
+
   let(:response) do 
     SerialSpec::ParsedBody.new(resource.to_json)
   end
 
-  context ".provide" do
-    context "resource supplied" do
+  # provide(model_instance) -> uses default ASM 
 
-      it "should match serializable object" do
-        provide(resource).to eq(true)
+  # provide(model_instance, as: ModelSerializer)
+  #context ".provide" do
+  #context "" do
+
+  #it "should match serializable object" do
+  #require'pry';binding.pry 
+  ##provide(resource).to eq(true)
+  #end
+  #end
+  #context "enumerable supplied" do
+
+  #end
+  #end
+
+  context "using provide" do
+    context "with :as" do
+      it "should match serialized model" do
+        expect(serialized_post)e.to provide(post, as: PostSerializer)
       end
     end
-    context "enumerable supplied" do
-
-    end
   end
+
 end
 
--- spec/support/fake_models.rb
+++ spec/support/fake_models.rb
@@ -1,3 +1,5 @@
+require 'active_model_serializers' 
+
 # inspired from ASM
 class Model
   def initialize(hash={})
@@ -13,16 +15,56 @@ class Model
   end
 end
 
+class Comment < Model
+  def active_model_serializer; CommentSerializer; end
+end
+
 class Post < Model
+  attr_accessor :comments, :comments_disabled, :author
+  def initialize(attributes)
+    super(attributes)
+    self.comments ||= []
+    self.author = nil
+  end
   def active_model_serializer; PostSerializer; end
+end
+
+class User < Model
+  def active_model_serializer; CommentSerializer; end
+end
+
+
+# SErializers
+
+class CommentSerializer
+  def initialize(comment, options={})
+    @object = comment
+  end
+
+  attr_reader :object
+
+  def serializable_hash
+    { :title => @object.read_attribute_for_serialization(:title) }
+  end
+
+  def as_json(options=nil)
+    options ||= {}
+    if options[:root] == false
+      serializable_hash
+    else
+      { :comment => serializable_hash }
+    end
+  end
 end
 
 class PostSerializer < ActiveModel::Serializer
   attributes :title, :body
   has_many :comments, :serializer => CommentSerializer
+  has_one :author, :serializer => UserSerialier
 end
 
-class Comment < Model
-    def active_model_serializer; CommentSerializer; end
+class UserSerializer < ActiveModel::Serializer
+  attributes :name
 end
+
 
